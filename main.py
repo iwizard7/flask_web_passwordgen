d62df9d3f4927d26c4fbde1a4934b91e66d44828
dmitrijsibanov <iwizard7@gmail.com>
2023-03-27 13:16:00 +0700

    generate requirements

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from flask import Flask, render_template
-import string
+from flask import Flask, render_template, request
 import random
+import string
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def password_generator():
+    if request.method == 'POST':
+        password_length = int(request.form['password_length'])
+        complexity = request.form['complexity']
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+        if complexity == 'easy':
+            characters = string.ascii_letters
+        elif complexity == 'medium':
+            characters = string.ascii_letters + string.digits
+        else: # complexity == 'hard'
+            characters = string.ascii_letters + string.digits + string.punctuation
 
-# background process happening without any refreshing
+        password = ''.join(random.choice(characters) for i in range(password_length))
+        return render_template('index.html', password=password)
 
-
-@app.route('/generate-password')
-def generate_password():
-       password_length = 10
-       password = ''.join(random.choices(string.ascii_letters + string.digits, k=password_length))
-       return render_template('generate_password.html', password=password)
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
