85a0857f6e076ccd12fd16ed91de2f56983deaf3
dmitrijsibanov <iwizard7@gmail.com>
2023-03-27 12:55:17 +0700

    generate ai scripts

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template
+import string
+import random
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def json():
+def home():
     return render_template('home.html')
 
 #background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
-    print ("Hello")
-    return ("nothing")
-
+@app.route('/generate-password')
+def generate_password():
+       password_length = 10
+       password = ''.join(random.choices(string.ascii_letters + string.digits, k=password_length))
+       return render_template('generate_password.html', password=password)
 
 if __name__ == '__main__':
     app.run(debug=True)
