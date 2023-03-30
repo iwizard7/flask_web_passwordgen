766fb85f74e45c3cc39cacd5aaba9cca8eb9c53b
dmitrijsibanov <iwizard7@gmail.com>
2023-03-30 14:39:04 +0700

    change main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, send_from_directory
-import random, os
+import random
+import os
 import string
 
 app = Flask(__name__)
 
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def password_generator():
     if request.method == 'POST':
@@ -17,13 +21,14 @@ def password_generator():
             characters = string.ascii_letters
         elif complexity == 'medium':
             characters = string.ascii_letters + string.digits
-        else: # complexity == 'hard'
+        else:  # complexity == 'hard'
             characters = string.ascii_letters + string.digits + string.punctuation
 
         password = ''.join(random.choice(characters) for i in range(password_length))
         return render_template('index.html', password=password)
 
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
