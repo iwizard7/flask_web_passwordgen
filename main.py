5fbb4d9c897eb171b2408681ea5d0e3b0b8f33e9
Dmitry <35992274+iwizard7@users.noreply.github.com>
2024-04-18 13:43:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+Flask-приложение для генерации паролей.
+
+Приложение принимает POST-запросы с параметрами 'password_length' и 'complexity'.
+На основе этих параметров генерируется пароль заданной длины и сложности.
+
+Параметры:
+- password_length - длина пароля.
+- complexity - сложность пароля ('easy', 'medium', 'hard').
+
+Возвращает:
+HTML-страницу с сгенерированным паролем.
+"""
+
 from flask import Flask, render_template, request, send_from_directory
 import random
 import os
@@ -8,27 +22,38 @@ app = Flask(__name__)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+ """
+ Функция favicon возвращает иконку приложения.
+
+ Иконка находится в директории 'static'.
+ """
+ return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def password_generator():
-    if request.method == 'POST':
-        password_length = int(request.form['password_length'])
-        complexity = request.form['complexity']
+ """
+ Функция password_generator генерирует пароль на основе параметров запроса.
 
-        if complexity == 'easy':
-            characters = string.ascii_letters
-        elif complexity == 'medium':
-            characters = string.ascii_letters + string.digits
-        else:  # complexity == 'hard'
-            characters = string.ascii_letters + string.digits + string.punctuation
+ Если метод запроса POST, то приложение генерирует пароль на основе параметров 'password_length' и 'complexity'.
+ В противном случае возвращается HTML-страница без пароля.
+ """
+ if request.method == 'POST':
+ password_length = int(request.form['password_length'])
+ complexity = request.form['complexity']
 
-        password = ''.join(random.choice(characters) for i in range(password_length))
-        return render_template('index.html', password=password)
+ if complexity == 'easy':
+ characters = string.ascii_letters
+ elif complexity == 'medium':
+ characters = string.ascii_letters + string.digits
+ else: # complexity == 'hard'
+ characters = string.ascii_letters + string.digits + string.punctuation
 
-    return render_template('index.html')
+ password = ''.join(random.choice(characters) for i in range(password_length))
+ return render_template('index.html', password=password)
+
+ return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+ app.run(host='0.0.0.0', port=8080)
