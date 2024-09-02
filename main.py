77ecfe7336d0a6cbc86f7e4042a8e6cfd023cb38
Dmitry <35992274+iwizard7@users.noreply.github.com>
2024-09-02 17:39:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,25 +18,27 @@ import string
 
 app = Flask(__name__)
 
-
 @app.route('/favicon.ico')
 def favicon():
- """
- Функция favicon возвращает иконку приложения.
+    """
+    Функция favicon возвращает иконку приложения.
 
- Иконка находится в директории 'static'.
- """
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
+    Иконка находится в директории 'static'.
+    """
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 @app.route('/', methods=['GET', 'POST'])
 def password_generator():
- """
- Функция password_generator генерирует пароль на основе параметров запроса.
+    """
+    Функция password_generator генерирует пароль на основе параметров запроса.
 
- Если метод запроса POST, то приложение генерирует пароль на основе параметров 'password_length' и 'complexity'.
- В противном случае возвращается HTML-страница без пароля.
- """
+    Если метод запроса POST, то приложение генерирует пароль на основе параметров 'password_length' и 'complexity'.
+    В противном случае возвращается HTML-страница без пароля.
+    """
     if request.method == 'POST':
         password_length = int(request.form['password_length'])
         complexity = request.form['complexity']
@@ -48,11 +50,10 @@ def password_generator():
         else:  # complexity == 'hard'
             characters = string.ascii_letters + string.digits + string.punctuation
 
-        password = ''.join(random.choice(characters) for i in range(password_length))
+        password = ''.join(random.choice(characters) for _ in range(password_length))
         return render_template('index.html', password=password)
 
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
